ca2906c8c8532c38f459cca3174482345d98eb4a
Richard Case <richard.case@outlook.com>
2018-10-08 10:05:02 +0100

    Added a great new line

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Hello from:  "+title+"\n")
+	fmt.Fprintln(w, "This is a new line")
 	fmt.Fprintln(w, "\n\nGoodbye :)")
 }
 
